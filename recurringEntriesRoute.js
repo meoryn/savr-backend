2d408e5ca20a9b8d5111db1703b6b1bc90325268
maximilianjandl <jandl.maximilian@gmail.com>
2025-06-24 17:12:18 +0200

    added recurring Entries pt2

---
+++ recurringEntriesRoute.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { supabase } from './supabaseClient.js';
+const router = express.Router();
+
+
+
+router.post('/recurringEntries', async (req, res) => {
+    const { user_id, category_id, amount, day } = req.body;
+    const refreshToken = req.headers["x-refresh-token"];
+    const token = req.headers.authorization?.split(" ")[1]
+    if (!token) return res.status(401).json({ error: "Missing token" })
+
+    // Session setzen
+    const { error: sessErr } = await supabase.auth.setSession({ access_token: token, refresh_token: refreshToken })
+    if (sessErr) return res.status(401).json({ error: "Invalid token", detailed: sessErr })
+
+
+    const { data, error } = await supabase
+        .from("recurring_entry")
+        .insert([
+            {
+                user_id: user_id,
+                category_id: category_id,
+                amount: amount,
+                day: day
+            },
+        ])
+
+
+
+    if (error) {
+        res.json(error);
+    } else {
+        res.send("Success");
+    }
+
+})
+
+
+export default router;
