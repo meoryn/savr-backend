ed54486f5175ef843f87ac1c77f6da25b2778ef9
maximilianjandl <jandl.maximilian@gmail.com>
2025-06-24 12:26:16 +0200

    fixed monthlyReport

--- monthlyReport.js
+++ monthlyReport.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import { supabase } from './supabaseClient.js';
+import { userMap, userIDtoAccountID } from './AccountIDHandler.js';
 const router = express.Router();
 
 
 
 router.get('/monthlyReport', async (req, res) => {
-    const account_id = req.body.account_id;
+    const user_id = req.body.user_id;
     const refreshToken = req.headers["x-refresh-token"];
-    console.log(account_id);
-
-    // Token auslesen
     const token = req.headers.authorization?.split(" ")[1]
     if (!token) return res.status(401).json({ error: "Missing token" })
 
@@ -17,12 +15,26 @@ router.get('/monthlyReport', async (req, res) => {
     const { error: sessErr } = await supabase.auth.setSession({ access_token: token, refresh_token: refreshToken })
     if (sessErr) return res.status(401).json({ error: "Invalid token", detailed: sessErr })
 
-    const { data, error } = await supabase
-    .from("monthly_report")
-    .select("sum, type, transaction_date, category_name")
-    .eq("account_id", account_id);
+    let account_id = userIDtoAccountID(user_id);
 
-    if(error) {
+    if (!account_id) {
+        const { data: accountData, error: accountError } = await supabase
+            .from("account")
+            .select("account_id")
+
+        if (accountError) console.log(accountError);
+        userMap.set(user_id, accountData[0].account_id);
+        account_id = accountData[0].account_id;
+    }
+    // Token auslesen
+
+
+    const { data, error } = await supabase
+        .from("monthly_report")
+        .select("sum, type, transaction_date, category_name")
+        .eq("account_id", account_id);
+
+    if (error) {
         res.json(error);
     }
     res.json(data);
