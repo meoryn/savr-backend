e409c67a9c98b16ee430611fed3657a6f15ee6eb
Manuel <elmer.manuel00@gmail.com>
2025-06-27 21:21:54 +0200

    added small fixes

--- AccountIDHandler.js
+++ AccountIDHandler.js
@@ -1,6 +1,9 @@
 export const userMap = new Map();
 
 export function userIDtoAccountID(user_id) {
+
+  console.log(userMap);
+
   if (userMap.has(user_id)) {
     return userMap.get(user_id);
   }
--- monthlyReport.js
+++ monthlyReport.js
@@ -6,39 +6,51 @@ const router = express.Router();
 
 
 router.post('/monthlyReport', async (req, res) => {
-    const user_id = req.body.user_id;
-    const refreshToken = req.headers["x-refresh-token"];
-    const token = req.headers.authorization?.split(" ")[1]
-    if (!token) return res.status(401).json({ error: "Missing token" })
-
-    // Session setzen
-    const { error: sessErr } = await supabase.auth.setSession({ access_token: token, refresh_token: refreshToken })
-    if (sessErr) return res.status(401).json({ error: "Invalid token", detailed: sessErr })
-
-    let account_id = userIDtoAccountID(user_id);
-
-    if (!account_id) {
-        const { data: accountData, error: accountError } = await supabase
-            .from("account")
-            .select("account_id")
-
-        if (accountError) console.log(accountError);
-        userMap.set(user_id, accountData[0].account_id);
-        account_id = accountData[0].account_id;
-    }
-    // Token auslesen
+  const user_id = req.body.user_id;
+  const refreshToken = req.headers["x-refresh-token"];
+  const token = req.headers.authorization?.split(" ")[1]
+  if (!token) return res.status(401).json({ error: "Missing token" });
+  if (!user_id) return res.status(401).json({ error: "Missing user_id" });
+  if (!refreshToken) return res.status(401).json({ error: "Missing refresh_token" });
 
 
-    const { data, error } = await supabase
-        .from("monthly_report")
-        .select("sum, type, transaction_date, category_name")
-        .eq("account_id", account_id)
-        .order("transaction_date");
 
-    if (error) {
-        res.json(error);
-    }
-    res.json(data);
+  // Session setzen
+  const { error: sessErr } = await supabase.auth.setSession({ access_token: token, refresh_token: refreshToken })
+  if (sessErr) return res.status(401).json({ error: "Invalid token", detailed: sessErr })
+
+  let account_id = userIDtoAccountID(user_id);
+
+  console.log('fetched accountID for ', user_id, ': ', account_id);
+
+  if (!account_id) {
+    const { data: accountData, error: accountError } = await supabase
+      .from("account")
+      .select("account_id")
+
+
+    console.log(accountData);
+
+    if (accountError) console.log(accountError);
+    userMap.set(user_id, accountData[0].account_id);
+    account_id = accountData[0].account_id;
+  }
+  // Token auslesen
+
+
+  const { data, error } = await supabase
+    .from("monthly_report")
+    .select("sum, type, transaction_date, category_name")
+    .eq("account_id", account_id)
+    .order("transaction_date");
+
+
+  console.log('Fetched Data', data);
+
+  if (error) {
+    res.json(error);
+  }
+  return res.json(data);
 
 })
 
